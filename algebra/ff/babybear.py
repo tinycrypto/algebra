df5def289a640f10aa3b0095b0d656c4a2f51c90
πa <76558220+rkdud007@users.noreply.github.com>
2025-02-22 17:03:13 +0900

    chore: clean (#3)
    clean up

--- algebra/ff/babybear.py
+++ algebra/ff/babybear.py
@@ -1,24 +1,6 @@
 from algebra.ff.prime_field import PrimeField
-from tinygrad.tensor import Tensor
 
 
 class BabyBear(PrimeField):
   # BabyBear prime: 2^31 - 2^27 + 1
   P = 2013265921
-
-  @classmethod
-  def mod_py_obj(cls, inp):
-    if isinstance(inp, Tensor):
-      return inp % cls.P
-    elif isinstance(inp, int):
-      return inp % cls.P
-    elif isinstance(inp, float):
-      return int(inp) % cls.P
-    else:
-      return [cls.mod_py_obj(x) for x in inp]
-
-  @classmethod
-  def modinv_impl(cls, x: Tensor) -> Tensor:
-    # Compute the modular inverse using Fermat's little theorem:
-    #   x^(P-2) mod P.
-    return cls.pow_tensor(x, cls.P - 2)
