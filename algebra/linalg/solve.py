b2667bbddf2d7b388c313736f88d3191ca8d43aa
Bhargav Annem <bannem@caltech.edu>
2025-03-03 15:48:21 -0800

    ruff

--- algebra/linalg/solve.py
+++ algebra/linalg/solve.py
@@ -1,13 +1,14 @@
 from tinygrad import Tensor
 
+
 def lu_decomposition(A):
-    n = A.shape[0]
-    L = Tensor.eye(n, dtype=A.dtype).contiguous()
-    U = Tensor.zeros((n, n), dtype=A.dtype).contiguous()
-    
-    for k in range(n):
-        U[k, k:] = A[k, k:] - L[k, :k] @ U[:k, k:]
-        if k < n-1:
-            L[k+1:, k] = (A[k+1:, k] - L[k+1:, :k] @ U[:k, k]) / U[k, k] 
-        
-    return L, U
+  n = A.shape[0]
+  L = Tensor.eye(n, dtype=A.dtype).contiguous()
+  U = Tensor.zeros((n, n), dtype=A.dtype).contiguous()
+
+  for k in range(n):
+    U[k, k:] = A[k, k:] - L[k, :k] @ U[:k, k:]
+    if k < n - 1:
+      L[k + 1 :, k] = (A[k + 1 :, k] - L[k + 1 :, :k] @ U[:k, k]) / U[k, k]
+
+  return L, U
